5ed0ca337676712f07202fe788377cd32e72fce7
Carlos Gabriel <gabrielcarlos0847@gmail.com>
2024-04-23 10:09:24 -0300

    adiciona setup

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "Carlos Gabriel"
+git config --global user.email "gabrielcarlos0847@gmail.com"
+
+cd C:\Users\CARLOSGABRIELGOMESLU\
+
+git clone https://github.com/Carllossz/Caesar-Chiper.git
+cd Caesar-Chiper
+
+code .
